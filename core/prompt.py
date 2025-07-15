4a8199a0f545c2dffb9b17138a835247ba39e6ec
AnaisBresle <anais.bresle@gmail.com>
2025-07-15 14:01:38 +0100

    Testing InquirePy input

--- core/prompt.py
+++ core/prompt.py
@@ -1,12 +1,33 @@
 from InquirerPy import prompt
+from InquirerPy.validator import NumberValidator
 
 questions = [
     {
-        'type': 'input',
-        'name': 'first_name',
-        'message': 'What\'s your first name',
-    }
+        "type": "input",
+        "message": "Enter your age:",
+        "validate": NumberValidator(),
+        "invalid_message": "Input should be number.",
+        "default": "18",
+        "name": "age",
+        "filter": lambda result: int(result),
+        "transformer": lambda result: "Adult" if int(result) >= 18 else "Youth",
+    },
+    {
+        "type": "rawlist",
+        "message": "What drinks would you like to buy:",
+        "default": 2,
+        "choices": lambda result: ["Soda", "Cidr", "Water", "Milk"]
+        if result["age"] < 18
+        else ["Wine", "Beer"],
+        "name": "drink",
+    },
+    {
+        "type": "list",
+        "message": "Would you like a bag:",
+        "choices": ["Yes", "No"],
+        "when": lambda result: result["drink"] in {"Wine", "Beer"},
+    },
+    {"type": "confirm", "message": "Confirm?", "default": True},
 ]
 
-answers = prompt(questions)
-print_json(answers)  # use the answers as input for your app 
+result = prompt(questions=questions)
