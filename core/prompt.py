21ca4d9773ff3808e6f3a43bb8d724fd4de07ea5
AnaisBresle <anais.bresle@gmail.com>
2025-07-15 15:17:50 +0100

    added licenses dropdown

--- core/prompt.py
+++ core/prompt.py
@@ -7,23 +7,46 @@ questions = [
         "message": "Enter the Project Title:",
         "validate": EmptyInputValidator(),
         "name": "title",
+        
     },
    
     {
         "type": "editor",  # multiline input in editor
         "name": "description",
-        "message": "Enter the Project Description:"
+        "message": "Provide a short description of the project:"
     },
-    {
-        "type": "editor",
-        "name": "installation",
-        "message": "Enter Installation Instructions:"
-    },
+   
     {
         "type": "editor",
         "name": "usage",
         "message": "Describe how to use this project and what are the expected outputs."
     },
+
+     {
+        "type": "editor",
+        "name": "installation",
+        "message": "Enter Installation Instructions:"
+    },
+
+     {
+        "type": "list",
+        "name": "license",
+        "message": "Choose a license:",
+        "choices": [
+            "No License — default copyright applies",
+            "Unlicensed — no permissions granted",
+            "Apache 2.0",
+            "Boost Software License 1.0",
+            "BSD 3-Clause",
+            "GNU AGPLv3",
+            "GNU GPLv2 (or later)",
+            "GNU LGPLv3",
+            "MIT",
+            "Mozilla Public License 2.0",
+
+        ],
+        "default": "MIT"
+    }
   
     {
         "type": "input",
@@ -34,7 +57,8 @@ questions = [
     {
         "type": "input",
         "name": "contact",
-        "message": "Contact Information (email, website, etc.):"
+        "message": "Contact Information (email, website, etc.):",
+        "validate": EmptyInputValidator(),
     }
 ]
 
