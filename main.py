9d4c2f5f45c74adf6205f8dcdfa12480c28d0af3
AnaisBresle <anais.bresle@gmail.com>
2025-07-16 12:49:18 +0100

    fixing NameError: name 'filename' is not defined

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from core.prompt import get_answers
 
-def write_readme(answers, filename="ReadMe-test.md"):
-    answers = get_answers()
+answers = get_answers()
+
+def write_readme(data, filename="ReadMe-test.md"):
+    
     with open(filename, "w", encoding="utf-8") as f:
-        for key, value in answers.items():
+        for key, value in data.items():
             f.write(f"{key.upper()}:\n{value}\n\n")
 
-    print(f"{filename} generated!")
+if __name__=="_main_":
+
+    write_readme(answers)
+
+print(f"{filename} generated!")
