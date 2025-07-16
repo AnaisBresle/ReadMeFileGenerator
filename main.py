7195d7429787db1e5e1d58129db186270d674592
AnaisBresle <anais.bresle@gmail.com>
2025-07-16 12:18:06 +0100

    write-readme file

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from core.prompt import get_answers
 
-answers = get_answers()
+def write_readme(data, filename="ReadMe-test.md"):
+    answers = get_answers()
+    with open(filename, "w", encoding="utf-8") as f:
+        for key, value in answers.items():
+            f.write(f"{key.upper()}:\n{value}\n\n")
+             print(f"{filename} generated!")
