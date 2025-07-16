2845eb2a93b67ff1a8ced314aaaaaca2e4b5d489
AnaisBresle <anais.bresle@gmail.com>
2025-07-16 17:10:27 +0100

    enumartion of INstalation steps

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ def write_readme(data, filename="ReadMe-test.md"):
         f.write(f"# {data['title'].strip()}\n\n")
         f.write(f"## Description\n{data['description'].strip()}\n\n") ## Adding .strip to remove any blank space, breaks etc..
         
+## Installation
         f.write(f"## Installation\n")
-        for line in data['installation'].splitlines():
-            f.write(f"• {line}\n")
+        for idx, line in enumerate(data['installation'].splitlines(), start=1):
+           line = line.strip()
+           if line:
+                f.write(f"{idx}. {line}\n")
         f.write("\n")
-
+       
+## Usage
         f.write("## Usage\n")
         f.write(f"{data['usage'].strip()}\n\n")
 
+## License & Contribution
         f.write("## License\n")
         f.write(f"{data['license'].strip()}\n\n")
         
@@ -24,6 +29,7 @@ def write_readme(data, filename="ReadMe-test.md"):
             link = data['contributing_link'].strip()
             f.write(f"[See CONTRIBUTING.md]({link}) for details on how to contribute.\n\n")
 
+## Author & Contact
         f.write("## Author\n")
         f.write(f"{data['author'].strip()}\n\n")
 
@@ -33,7 +39,7 @@ def write_readme(data, filename="ReadMe-test.md"):
             if line:
                 f.write(f"• {line}\n")
             f.write("\n")
-
+## Links
         f.write(f"## Useful Links\n")
         for line in data['links'].splitlines():
             line = line.strip()
